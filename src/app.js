ced8ea4f7e67f30c81c8645629c080a5659fc55f
Tushar Tambe <44019295+tushartambe@users.noreply.github.com>
2019-01-19 10:47:01 +0530

    removed comments.json from git.

--- src/app.js
+++ src/app.js
@@ -1,14 +1,6 @@
 const fs = require("fs");
 let allComments = require("../comments.json");
 
-const html = {
-  homePage: (req, res) => {
-    fs.readFile("./src/homepage.html", (err, content) => {
-      send(res, content, 200);
-    });
-  }
-};
-
 const send = (res, content, statusCode = 200) => {
   res.statusCode = statusCode;
   res.write(content);
